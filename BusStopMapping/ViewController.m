70fdec0da76365f420a75c4cbc7db7b0b66dd259
John Andrews <johnandrews@Johns-MacBook.local>
2013-10-08 14:41:09 -0500

    I'm able to pull in all the bustop info I want

--- BusStopMapping/ViewController.m
+++ BusStopMapping/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSMutableDictionary* dictionaryFirstJSONLayer;
+    NSMutableArray* arraySecondJSONLayer;
+    NSMutableDictionary* dictionaryThirdJSONLayer;
+    NSMutableDictionary* dictionaryFourthJSONLayer;
+    
+    NSMutableString* busStopName;
+    NSMutableString* busStopLatitude;
+    NSMutableString* busStopLongitude;
+}
 
 @end
 
@@ -16,14 +26,38 @@
 
 - (void)viewDidLoad
 {
+    NSURL* url = [NSURL URLWithString:@"http://mobilemakers.co/lib/bus.json"];
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+     {
+         dictionaryFirstJSONLayer = [[NSMutableDictionary alloc] initWithDictionary:[NSJSONSerialization JSONObjectWithData:data options:0 error:&connectionError]];
+         
+         arraySecondJSONLayer = [[NSMutableArray alloc] initWithArray:[dictionaryFirstJSONLayer objectForKey:@"row"]];
+         
+         for (dictionaryThirdJSONLayer in arraySecondJSONLayer) {
+             busStopName = [dictionaryThirdJSONLayer objectForKey:@"cta_stop_name"];
+             
+             dictionaryFourthJSONLayer = [[NSMutableDictionary alloc] initWithDictionary:[dictionaryThirdJSONLayer objectForKey:@"location"]];
+             busStopLatitude = [dictionaryFourthJSONLayer objectForKey:@"latitude"];
+             busStopLongitude = [dictionaryFourthJSONLayer objectForKey:@"longitude"];
+             
+             //NSLog(@" the different bus top names are %@", busStopName);
+             //NSLog(@" the different bus top latitudes are %@", busStopLatitude);
+             //NSLog(@" the different bus top longitudes are %@", busStopLongitude);
+         }
+         
+     }];
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
+
+
 
 @end
